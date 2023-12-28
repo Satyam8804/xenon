1bbef70fbeb79d91def82f21f0367c3aaa25a090
Satyam Kumar <satyam8804378323@gmail.com>
2023-12-28 20:24:02 +0530

    new commit

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,8 +7,6 @@ const ProductCard = ({ product }) => (
     <img src={product.image} alt={product.title} />
     <div className="card-body">
       <h5 className="card-title">{product.title}</h5>
-      <p className="card-text">{product.description}</p>
-      <p className="card-text">${product.price}</p>
     </div>
   </div>
 );
